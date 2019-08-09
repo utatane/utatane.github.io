09aa164b0840ad827524bbebede155b675add725
KBT1021 <azabu19yuuki95@yahoo.co.jp>
2019-08-09 20:41:26 +0900

    html修正．themeとconceptに分けて文章の重複をなくした

--- javascript/utatane_index.js
+++ javascript/utatane_index.js
@@ -28,10 +28,10 @@ function header_index_scroll(len, width)
         header_index_scroll += '<div class="container">';
             header_index_scroll += '<div class="row header_scroll_pc">';
                 //header_index_scroll += '<div class="col-xs-4 utatane_logo"><a href="https://mayfes2018-utatane.com"><img src="./pictures/UTaTane_icon_moji.png" alt="UTaTane"></a></div>';
-                header_index_scroll += '<div class="col-xs-4 utatane_logo"><a href=';
+                header_index_scroll += '<div class="col-xs-3 utatane_logo"><a href=';
                 header_index_scroll += ref;
                 header_index_scroll += '><img src="./pictures/UTaTane_icon_moji.png" alt="UTaTane"></a></div>';
-                header_index_scroll += '<div class="col-xs-8 index_nav"><div><a href="#top">Top</a> | <a href="#about">About</a> | <a href="#concept">Concept</a> | <a href="#news">News</a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
+                header_index_scroll += '<div class="col-xs-9 index_nav"><div><a href="#top">Top</a> | <a href="#concept">Concept</a> | <a href="#theme">Theme</a> | <a href="#news"> News </a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
             header_index_scroll += '</div>';
         header_index_scroll += '</div>';
 
@@ -75,10 +75,10 @@ function header_index() {
 
                 html += '<div class="row header_pc">';
 //                    html += '<div class="col-xs-4 utatane_icon"><a href="https://mayfes2018-utatane.com"><img src="./pictures/UTaTane_icon.png" alt="UTaTane"></a></div>';
-                    html += '<div class="col-xs-4 utatane_icon"><a href=';
+                    html += '<div class="col-xs-3 utatane_icon"><a href=';
                     html += ref;
                     html += '><img src="./pictures/UTaTane_icon.png" alt="UTaTane"></a></div>';
-                    html += '<div class="col-xs-8 index_nav"><div><a href="#top">Top</a> | <a href="#about">About</a> | <a href="#concept">Concept</a> | <a href="#news">News</a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
+                    html += '<div class="col-xs-9 index_nav"><div><a href="#top">Top</a> | <a href="#concept">Concept</a> | <a href="#theme">Theme</a> | <a href="#news"> News </a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
                 html += '</div>'; // header_pc
 
             html += '</div>';
@@ -96,12 +96,11 @@ function menulist_index_appear()
   menulist_index = '';
   menulist_index += '<table>';
     menulist_index += '<tr><td><a href="#top" onclick="menulist_index_disappear()">Top</tr></a></td>';
-    menulist_index += '<tr><td><a href="#about" onclick="menulist_index_disappear()">About</tr></a></td>';
     menulist_index += '<tr><td><a href="#concept" onclick="menulist_index_disappear()">Concept</tr></a></td>';
+    menulist_index += '<tr><td><a href="#theme" onclick="menulist_index_disappear()">Theme</tr></a></td>';
     menulist_index += '<tr><td><a href="#news" onclick="menulist_index_disappear()">News</tr></a></td>';
     menulist_index += '<tr><td><a href="#works" onclick="menulist_index_disappear()">Works</tr></a></td>';
     menulist_index += '<tr><td><a href="#contact" onclick="menulist_index_disappear()">Contact</tr></a></td>';
-    menulist_index += '<tr><td><a href="#links" onclick="menulist_index_disappear()">Links</tr></a></td>';
   menulist_index += '</table>';
 
   document.getElementById("menubutton").innerHTML = '';
