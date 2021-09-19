387d28bfab6c0fd74a43d7f1fb9b728848b8af94
KBT1021 <azabu19yuuki95@yahoo.co.jp>
2021-09-20 01:02:19 +0900

    Update link structures

--- javascript/utatane_21MayFes_contents.js
+++ javascript/utatane_21MayFes_contents.js
@@ -31,8 +31,9 @@ function header_index_scroll(len, width)
                 header_index_scroll += '<div class="col-xs-3 utatane_logo"><a href=';
                 header_index_scroll += ref;
                 header_index_scroll += '><img src="../../pictures/UTaTane_icon_moji.png" alt="UTaTane"></a></div>';
-                header_index_scroll += '<div class="col-xs-12 index_nav"><div><a href="./index.html">トップへ戻る</a> | <a href="./discussion.html">展示秘話・議論</a> | <a href="./distorted_heading.html">ねじまげ見出し</a> |'
-                header_index_scroll += ' <a href="./drawing_song_miscommunication.html"> すれ違いえかき </a> | <a href="./onomatopeia_emoticon.html"> オノマトペカタログ </a> | <a href="./polyphonic_scape.html"> ポリフォニックスケープ </a>| <a href="./unit.html">数の表現事典</a> | <a href="./workshop.html">ワークショップ</a> |</div></div>';
+                header_index_scroll += '<div class="col-xs-12 index_nav"><div><a href="./index.html">他の展示を探す</a> | <a href="./discussion.html">展示体験サイトへ戻る</a>';
+                //header_index_scroll += '<div class="col-xs-12 index_nav"><div><a href="./index.html">トップへ戻る</a> | <a href="./discussion.html">展示秘話・議論</a> | <a href="./distorted_heading.html">ねじまげ見出し</a> |'
+                //header_index_scroll += ' <a href="./drawing_song_miscommunication.html"> すれ違いえかき </a> | <a href="./onomatopeia_emoticon.html"> オノマトペカタログ </a> | <a href="./polyphonic_scape.html"> ポリフォニックスケープ </a>| <a href="./unit.html">数の表現事典</a> | <a href="./workshop.html">ワークショップ</a> |</div></div>';
             header_index_scroll += '</div>';
         header_index_scroll += '</div>';
 
@@ -79,8 +80,9 @@ function header_index() {
                   html += '<div class="col-xs-3 utatane_icon"><a href=';
                   html += ref;
                   html += '><img src="../../pictures/UTaTane_icon.png" alt="UTaTane"></a></div>';
-                  html += '<div class="col-xs-12 index_nav"><div><a href="./index.html">トップへ戻る</a> | <a href="./discussion.html">展示秘話・議論</a> | <a href="./distorted_heading.html">ねじまげ見出し</a> |'
-                  html += ' <a href="./drawing_song_miscommunication.html"> すれ違いえかき </a> | <a href="./onomatopeia_emoticon.html"> オノマトペカタログ </a> | <a href="./polyphonic_scape.html"> ポリフォニックスケープ </a>| <a href="./unit.html">数の表現事典</a> | <a href="./workshop.html">ワークショップ</a> |</div></div>';
+                  html += '<div class="col-xs-12 index_nav"><div><a href="./index.html">他の展示を探す</a> |  <a href="#" onclick="window.history.back(); return false;">展示体験サイトへ戻る</a>';
+                  //html += '<div class="col-xs-12 index_nav"><div><a href="./index.html">トップへ戻る</a> | <a href="./discussion.html">展示秘話・議論</a> | <a href="./distorted_heading.html">ねじまげ見出し</a> |'
+                  //html += ' <a href="./drawing_song_miscommunication.html"> すれ違いえかき </a> | <a href="./onomatopeia_emoticon.html"> オノマトペカタログ </a> | <a href="./polyphonic_scape.html"> ポリフォニックスケープ </a>| <a href="./unit.html">数の表現事典</a> | <a href="./workshop.html">ワークショップ</a> |</div></div>';
           html += '</div>';
       html += '</div>';
   html += '</header>';
@@ -94,14 +96,15 @@ function menulist_index_appear()
 
   menulist_index = '';
   menulist_index += '<table>';
-    menulist_index += '<tr><td><a href="./index.html" onclick="menulist_index_disappear()">トップへ戻る</tr></a></td>';
-    menulist_index += '<tr><td><a href="./discussion.html" onclick="menulist_index_disappear()">展示秘話・議論</tr></a></td>';
-    menulist_index += '<tr><td><a href="./distorted_heading.html" onclick="menulist_index_disappear()">ねじまげ見出し</tr></a></td>';
+    menulist_index += '<tr><td><a href="./index.html" onclick="menulist_index_disappear()">他の展示を探す</tr></a></td>';
+    menulist_index += '<tr><td><a href="#" onclick="window.history.back(); return false;">展示体験サイトへ戻る</tr></a></td>';
+    //menulist_index += '<tr><td><a href="./discussion.html" onclick="menulist_index_disappear()">展示秘話・議論</tr></a></td>';
+    /*menulist_index += '<tr><td><a href="./distorted_heading.html" onclick="menulist_index_disappear()">ねじまげ見出し</tr></a></td>';
     menulist_index += '<tr><td><a href="./drawing_song_miscommunication.html" onclick="menulist_index_disappear()">すれ違いえかき</tr></a></td>';
     menulist_index += '<tr><td><a href="./onomatopeia_emoticon.html" onclick="menulist_index_disappear()">オノマトペカタログ</tr></a></td>';
     menulist_index += '<tr><td><a href="./polyphonic_scape.html" onclick="menulist_index_disappear()">ポリフォニック・スケープ</tr></a></td>';
     menulist_index += '<tr><td><a href="./unit.html" onclick="menulist_index_disappear()">数の表現事典</tr></a></td>';
-    menulist_index += '<tr><td><a href="./workshop.html" onclick="menulist_index_disappear()">ワークショップ</tr></a></td>';
+    menulist_index += '<tr><td><a href="./workshop.html" onclick="menulist_index_disappear()">ワークショップ</tr></a></td>';*/
   menulist_index += '</table>';
 
   document.getElementById("menubutton").innerHTML = '';
