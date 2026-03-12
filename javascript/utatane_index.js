cbf5552756497421a13b4dfd72b741f7cdad33fc
KBT1021 <azabu19yuuki95@yahoo.co.jp>
2026-03-12 22:38:03 +0900

    Update index.html

--- javascript/utatane_index.js
+++ javascript/utatane_index.js
@@ -31,7 +31,7 @@ function header_index_scroll(len, width)
                 header_index_scroll += '<div class="col-xs-3 utatane_logo"><a href=';
                 header_index_scroll += ref;
                 header_index_scroll += '><img src="./pictures/UTaTane_icon_moji.png" alt="UTaTane"></a></div>';
-                header_index_scroll += '<div class="col-xs-9 index_nav"><div><a href="#top">Top</a> | <a href="#concept">Concept</a> | <a href="#theme">Theme</a> | <a href="#news"> News </a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
+                header_index_scroll += '<div class="col-xs-9 index_nav"><div><a href="#top">Top</a> | <a href="#concept">Concept</a> | <a href="#news"> News </a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
             header_index_scroll += '</div>';
         header_index_scroll += '</div>';
 
@@ -79,7 +79,7 @@ function header_index() {
                     html += '<div class="col-xs-3 utatane_icon"><a href=';
                     html += ref;
                     html += '><img src="./pictures/UTaTane_icon.png" alt="UTaTane"></a></div>';
-                    html += '<div class="col-xs-9 index_nav"><div><a href="#top">Top</a> | <a href="#concept">Concept</a> | <a href="#theme">Theme</a> | <a href="#news"> News </a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
+                    html += '<div class="col-xs-9 index_nav"><div><a href="#top">Top</a> | <a href="#concept">Concept</a> | <a href="#news"> News </a> | <a href="#works">Works</a> | <a href="#contact">Contact</a></div></div>';
                 html += '</div>'; // header_pc
 
             html += '</div>';
@@ -98,7 +98,7 @@ function menulist_index_appear()
   menulist_index += '<table>';
     menulist_index += '<tr><td><a href="#top" onclick="menulist_index_disappear()">Top</tr></a></td>';
     menulist_index += '<tr><td><a href="#concept" onclick="menulist_index_disappear()">Concept</tr></a></td>';
-    menulist_index += '<tr><td><a href="#theme" onclick="menulist_index_disappear()">Theme</tr></a></td>';
+    //menulist_index += '<tr><td><a href="#theme" onclick="menulist_index_disappear()">Theme</tr></a></td>';
     menulist_index += '<tr><td><a href="#news" onclick="menulist_index_disappear()">News</tr></a></td>';
     menulist_index += '<tr><td><a href="#works" onclick="menulist_index_disappear()">Works</tr></a></td>';
     menulist_index += '<tr><td><a href="#contact" onclick="menulist_index_disappear()">Contact</tr></a></td>';
